20c0b8a9b0b188b877bb3ff48612434930faf604
nishet <nishet@INLEN8520042005.corp.capgemini.com>
2022-03-15 22:36:58 +0530

    first commit

---
+++ BugTracker/src/main/java/com/sprint/BugTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerApplication.class, args);
+		System.out.println("fine");
+	}
+
+}
